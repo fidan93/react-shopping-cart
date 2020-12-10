724cfaf340edfc0dd195dc3925425a1843876cad
Fidan Novruzova <yf@Fidans-MacBook-Pro.local>
2020-12-10 13:59:05 -0800

    Met MVP and remove btn stretch

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import {CartContext} from "../contexts/CartContext";
 
 const Item = props => {
+	const {cart,setCart} = useContext(CartContext);
+	
+	console.log(props)
+
+	const removeItem = () => {
+		
+	const removed = cart.filter(item => {
+		   console.log(item.id,props.id)
+        return item.id!==props.id
+	   })
+	  console.log(removed)
+	  setCart(removed)
+	}
+
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
@@ -9,7 +24,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={removeItem}>Remove from cart</button>
 			</div>
 		</div>
 	);
